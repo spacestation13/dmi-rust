0695b585342cf2b35529e8e470c5689b0cb9107b
ZeWaka <zewakagamer@gmail.com>
2023-12-23 17:48:54 -0800

    Add public direction structs (#18)

---
+++ src/dirs.rs
@@ -0,0 +1,38 @@
+use bitflags::bitflags;
+
+bitflags! {
+	/// The possible values for a direction in DM.
+	pub struct Dirs: u8 {
+		const NORTH =	1 << 0;
+		const SOUTH =	1 << 1;
+		const EAST =	1 << 2;
+		const WEST =	1 << 3;
+		const SOUTHEAST = Self::SOUTH.bits() | Self::EAST.bits();
+		const SOUTHWEST = Self::SOUTH.bits() | Self::WEST.bits();
+		const NORTHEAST = Self::NORTH.bits() | Self::EAST.bits();
+		const NORTHWEST = Self::NORTH.bits() | Self::WEST.bits();
+	}
+}
+
+/// A list of every cardinal direction.
+pub const CARDINAL_DIRS: [Dirs; 4] = [Dirs::NORTH, Dirs::SOUTH, Dirs::EAST, Dirs::WEST];
+
+/// A list of every ordinal direction.
+pub const ORDINAL_DIRS: [Dirs; 4] = [
+	Dirs::NORTHEAST,
+	Dirs::NORTHWEST,
+	Dirs::SOUTHEAST,
+	Dirs::SOUTHWEST,
+];
+
+/// A list of every direction, cardinals and ordinals.
+pub const ALL_DIRS: [Dirs; 8] = [
+	Dirs::NORTH,
+	Dirs::SOUTH,
+	Dirs::EAST,
+	Dirs::WEST,
+	Dirs::NORTHEAST,
+	Dirs::NORTHWEST,
+	Dirs::SOUTHEAST,
+	Dirs::SOUTHWEST,
+];
